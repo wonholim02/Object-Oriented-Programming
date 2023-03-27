79462cdaf1c989b141c4a1f2ae336499511e5443
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2023-03-27 15:13:55 -0400

    Update SubstractionGame.java

--- General-Coding-Basics/Java/Game/SubstractionGame.java
+++ General-Coding-Basics/Java/Game/SubstractionGame.java
@@ -1,6 +1,5 @@
 /**************************************************************************
 Name: Wonho Lim 
-TYPE: Bugil Academy
 Description: This program is a mini substraction game that provides several 
 games and highest score at last. 3~6 six random numbers are given to players 
 and players should calculate them properly to earn a point.  
